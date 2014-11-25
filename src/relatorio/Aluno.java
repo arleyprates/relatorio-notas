1ea4bb6b7d904b34390bbcf48d77713b24045019
Marinaldo <marinaldo-barbosa@hotmail.com>
2014-11-25 13:41:26 -0300

    Update Aluno.java

--- src/relatorio/Aluno.java
+++ src/relatorio/Aluno.java
@@ -30,4 +30,20 @@ public class Aluno extends Avaliacao{
 		this.colegiado = colegiado;
 	}
 	
+	public static void main(String[] args) {
+		Aluno aluno1 = new Aluno();
+		aluno1.matricula = Integer.parseInt("201312453");
+		aluno1.nome = "Mateus Albuquerque dos Anjos";
+		aluno1.colegiado = 192;
+		
+
+		Aluno aluno2 = new Aluno();
+		aluno2.matricula = Integer.parseInt("201312453");
+		aluno2.nome = "Mateus Henrique dos Santos";
+		aluno2.colegiado = 187;
+		
+		System.out.println(aluno1.matricula + " " + aluno1.nome + " " + aluno1.colegiado);
+		System.out.println(aluno2.matricula + " " + aluno2.nome + " " + aluno2.colegiado);
+	}
+	
 }
