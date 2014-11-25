906e3e919f6b3ba7e0bc95833e2642e45c972429
Arley Prates <arleyprates@gmail.com>
2014-11-25 00:40:10 -0300

    Add classe Avaliacao

---
+++ src/relatorio/Avaliacao.java
@@ -0,0 +1,26 @@
+package relatorio;
+
+public class Avaliacao {
+	private String nome;
+	private float nota;
+	private float peso;	
+	
+	public String getNomeAvaliacao() {
+		return nome;
+	}
+	public void setNomeAvaliacao(String nome) {
+		this.nome = nome;
+	}
+	public float getNota() {
+		return nota;
+	}
+	public void setNota(float nota) {
+		this.nota = nota;
+	}
+	public float getPeso() {
+		return peso;
+	}
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+}
