ebe947a0be2a7616a991b92529a3328f17cffb5b
Arley Prates <arleyprates@gmail.com>
2014-11-26 23:25:06 -0300

    Ajustando classe Avaliacao

--- src/relatorio/Avaliacao.java
+++ src/relatorio/Avaliacao.java
@@ -1,55 +1,35 @@
 package relatorio;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Avaliacao {
 	
 	private float nota;
 	private String nomeAvaliacao;
 	private float peso;
 	
-	public void setAvaliacao(String nome, float nota, float peso) {
-		Map<String, List<Float>> mapaNotas = new HashMap<String, List<Float>>();
-		List<Float> setNota = new ArrayList<Float>();
-	
-		setNota.add(nota);
-		setNota.add(peso);
-		mapaNotas.put(nome, setNota);
-		
-		for (Map.Entry<String, List<Float>> entry : mapaNotas.entrySet()) {
-		
-			String key = entry.getKey();
-			
-			List<Float> values = entry.getValue();
-			
-			this.nomeAvaliacao = key;
-			this.nota = values.get(0);
-			this.peso = values.get(1);
-			
-			//System.out.println("Key = " + key);
-			//System.out.println("Values =" + values );
-		}
-	}
-	
-	public void getAvaliacao() {
-		System.out.println(this.nomeAvaliacao);
-		System.out.println(this.nota);
-		System.out.println(this.peso);
-	}
-	
 	public float getNota() {
-		return this.nota;
+		return nota;
+	}
+	
+	public void setNota(float nota) {
+		this.nota = nota;
 	}
 	
 	public String getNomeAvaliacao() {
-		return this.nomeAvaliacao;
+		return nomeAvaliacao;
+	}
+	
+	public void setNomeAvaliacao(String nomeAvaliacao) {
+		this.nomeAvaliacao = nomeAvaliacao;
 	}
 	
 	public float getPeso() {
-		return this.peso;
+		return peso;
 	}
+	
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+	
+	
 	
 }
