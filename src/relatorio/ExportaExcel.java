11e98c76fb945b113292cbbc198fc61a28c4d057
Arley Prates <arleyprates@gmail.com>
2014-11-28 01:26:10 -0300

    Ajustando for para leitura de matriculas alunos e medias

--- src/relatorio/ExportaExcel.java
+++ src/relatorio/ExportaExcel.java
@@ -13,25 +13,45 @@ public class ExportaExcel {
 	
 	public static void main(String[] args) throws IOException, BiffException {
 		
-		Workbook workbook = Workbook.getWorkbook(new File("c:/teste.xls"));//Colocar o arquivo de excel correto
+		Workbook workbook = Workbook.getWorkbook(new File("files/planilha.xls"));//Colocar o arquivo de excel correto
 		
 		Sheet sheet = workbook.getSheet(0);
 		
 		int linhas = sheet.getRows();
-				
-			for(int i = 0; i < linhas; i++){
+		boolean a = false;
+		
+		for(int i = 0; i < linhas; i++){			
 			
-				Cell a1 = sheet.getCell(0,i);
-				Cell a2 = sheet.getCell(1,i);
-				Cell a3 = sheet.getCell(2,i);			
-				String as1 = a1.getContents();
-				String as2 = a2.getContents();
-				String as3 = a3.getContents();
-				System.out.println("Valor da Coluna 1:" +  as1);
-				System.out.println("Valor da Coluna 2:" +  as2);
-				System.out.println("Valor da Coluna 3:" +  as3);		
+			Cell a1 = sheet.getCell(0,i);
+			String as1 = a1.getContents();
+			
+			if (as1.equals("1")){
+				a = true;
 			}
-					
+				
+			if (a){
+				Cell a2 = sheet.getCell(1,i);
+				String as2 = a2.getContents();
+				//instancia matricula
+				Cell a3 = sheet.getCell(2,i);
+				String as3 = a3.getContents();
+				//instancia nome
+				Cell a4 = sheet.getCell(3,i);
+				String as4 = a4.getContents();
+				
+				Cell a5 = sheet.getCell(4,i);
+				String as5 = a5.getContents();
+				
+				System.out.println("Matricula:" +  as2);
+				System.out.println("Nome:" +  as3);
+				System.out.println("Nota:" +  as4);
+				System.out.println("Nota:" +  as5);
+			}
+						
+		}
+		
 		workbook.close();
+	
 	}
+	
 }
