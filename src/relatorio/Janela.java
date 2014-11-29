1c3f29115c6bddb8c0ef02009589cc040fceb0cb
Marinaldo <marinaldo-barbosa@hotmail.com>
2014-11-28 22:44:34 -0300

    Redução para apenas 2 botões simplérrimos
    Botões de funções específicas. Esta classe tem um atributo do tipo string que pega o endereço do arquivo.

--- src/relatorio/Janela.java
+++ src/relatorio/Janela.java
@@ -1,57 +1,92 @@
 package relatorio;
 
-import java.awt.BorderLayout;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextPane;
-import javax.swing.SwingUtilities;
-import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
 @SuppressWarnings("serial")
 public class Janela extends JFrame {
+	/**
+	* Tudo o que esta janela faz é abrir uma janela pequena com apenas dois botões de funções específicas
+	*
+	*O primeiro abre uma janela de diálogo de busca em diretório do sistema.
+	*Retorna uma String com o endereço do arquivo. 
+	*
+	*O segundo botão simplesmente cancela e fecha o programa.
+	**/
+	
+	private String palavra;
+	
+	public String getPalavra (){
+		return palavra;
+	}
+	
+	public void setPalavra (String palavra){
+		this.palavra = palavra;
+	}
+
+	public File abrePath() {
+		JFileChooser arquivo = new JFileChooser();
+
+		// configurando para que somente arquivos seja abertos.
+		arquivo.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+		SwingUtilities.updateComponentTreeUI(arquivo);
+		
+		// armazena a escolha do usuário.
+        int respostaDeAbrindoArquivo = arquivo.showOpenDialog(Janela.this);
+		if (respostaDeAbrindoArquivo == JFileChooser.APPROVE_OPTION) {
+			return arquivo.getSelectedFile();
+        }
+        return null;
+	}
 
 	public Janela() {
-		BorderLayout borderLayout = (BorderLayout) getContentPane().getLayout();
-		borderLayout.setVgap(30);
-		borderLayout.setHgap(30);
+		//final setsize(261,186)
+		setSize(261,186);
+		setAutoRequestFocus(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
 		setTitle("Configuração");
-		
-		JTextPane txtpnCliqueNoBoto = new JTextPane();
-		txtpnCliqueNoBoto.setToolTipText("Aqui vai o endereço para a sua planilha de notas. \nVocê pode digitar ou clicar no botão escolher arquivos para selecioná-lo.\nOu clique em fechar para cancelar.");
-		txtpnCliqueNoBoto.setText("Clique no botão escolher arquivo");
-		txtpnCliqueNoBoto.addFocusListener(new FocusAdapter() {
-			@Override
-			public void focusGained(FocusEvent e) {
-			}
-		});
-		getContentPane().add(txtpnCliqueNoBoto, BorderLayout.CENTER);
-		
-		JButton btnOk = new JButton("Ok");
-		btnOk.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseReleased(MouseEvent e) {
-			}
-		});
-		getContentPane().add(btnOk, BorderLayout.SOUTH);
-		
-		JButton btnEscolher = new JButton("Escolher");
-		btnEscolher.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseReleased(MouseEvent e) {
-			}
-		});
-		getContentPane().add(btnEscolher, BorderLayout.EAST);
-		
-		JLabel lblEscolhaOArquivo = new JLabel("Escolha o arquivo:");
+		getContentPane().setLayout(new GridLayout(0, 1, 20, 20));
+				
+		JLabel lblEscolhaOArquivo = new JLabel("Bem vindo ao programa.");
+		lblEscolhaOArquivo.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblEscolhaOArquivo.setFont(new Font("Times New Roman", Font.BOLD, 18));
 		lblEscolhaOArquivo.setHorizontalAlignment(SwingConstants.CENTER);
-		getContentPane().add(lblEscolhaOArquivo, BorderLayout.NORTH);
+		getContentPane().add(lblEscolhaOArquivo);
 		
+		JButton btnEscolherArquivo = new JButton("Escolher arquivo");
+		btnEscolherArquivo.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		lblEscolhaOArquivo.setLabelFor(btnEscolherArquivo);
+		btnEscolherArquivo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				palavra = abrePath().getAbsolutePath();
+				//System.out.println(palavra);
+			}
+		});
+		btnEscolherArquivo.setToolTipText("Clique aqui e selecione o seu arquivo. Ou feche para encerrar o programa.");
+		getContentPane().add(btnEscolherArquivo);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.exit(EXIT_ON_CLOSE);
+			}
+		});
+		btnCancelar.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		getContentPane().add(btnCancelar);
 	}
 
 	public static void main(String[] args) {
@@ -62,6 +97,6 @@ public class Janela extends JFrame {
 				pp = new Janela();
 				pp.setVisible(true);
 			}
-		});
+		});		
 	}
 }
