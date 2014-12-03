bf0fafdceaca397a0f24df4038f1db9357d3b8ed
rafaelsandes <rafa_sandes@hotmail.com>
2014-12-03 15:10:56 -0300

    Update Avaliacao.java
    Modifiquei para pegar as notas das 3 avaliações. Notei também que estava duplicando a primeira nota. Mais Já resolve e está funcionando Normal

--- src/relatorio/Avaliacao.java
+++ src/relatorio/Avaliacao.java
@@ -2,17 +2,36 @@ package relatorio;
 
 public class Avaliacao {
 	
-	private float nota;
+	private float nota1;
+	private float nota2;
+	private float nota3;
 	private String nomeAvaliacao;
 	private float peso;
 	
-	public float getNota() {
-		return nota;
+	public float getNota1() {
+		return nota1;
 	}
 	
-	public void setNota(float nota) {
-		this.nota = nota;
+	public void setNota1(float nota) {
+		this.nota1 = nota;
 	}
+	
+	public float getNota2() {
+		return nota2;
+	}
+	
+	public void setNota2(float nota) {
+		this.nota2 = nota;
+	}
+	
+	public float getNota3() {
+		return nota3;
+	}
+	
+	public void setNota3(float nota) {
+		this.nota3 = nota;
+	}
+	
 	
 	public String getNomeAvaliacao() {
 		return nomeAvaliacao;
@@ -29,7 +48,6 @@ public class Avaliacao {
 	public void setPeso(float peso) {
 		this.peso = peso;
 	}
-	
 	
 	
 }
