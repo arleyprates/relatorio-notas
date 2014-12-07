7eb7086c8f44d6a4ba664b0dc4d7169429c4da70
Arley Prates <arleyprates@gmail.com>
2014-12-07 17:03:44 -0300

    Teste planilha OK

--- src/relatorio/Principal.java
+++ src/relatorio/Principal.java
@@ -4,8 +4,10 @@ import report.PlanilhaReport;
 
 public class Principal {
 
-	public static void mainly(String args) {
+	//public static void mainly(String args) {
 		// TODO Auto-generated method stub
+		public static void main(String[] args) {
+			
 		
 		PlanilhaReport pr = new PlanilhaReport();
 		
