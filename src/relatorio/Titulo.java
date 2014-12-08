fce0f05e5237eb89109dacdc1441a247a84e593b
Arley Prates <arleyprates@gmail.com>
2014-12-08 13:49:44 -0300

    Add properties para definir informacoes to titulo dinamicamente

---
+++ src/relatorio/Titulo.java
@@ -0,0 +1,65 @@
+package relatorio;
+
+public class Titulo {
+	private String universidade;
+	private String numeroRegistro;
+	private String departamento;
+	private String disciplina;
+	private String periodoLetivo;
+	private String turma;
+	private String cargaHoraria;
+	
+	public String getUniversidade() {
+		return universidade;
+	}
+	
+	public void setUniversidade(String universidade) {
+		this.universidade = universidade;
+	}
+	
+	public String getNumeroRegistro() {
+		return numeroRegistro;
+	}
+	public void setNumeroRegistro(String numeroRegistro) {
+		this.numeroRegistro = numeroRegistro;
+	}
+	
+	public String getDepartamento() {
+		return departamento;
+	}
+	
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+	
+	public String getDisciplina() {
+		return disciplina;
+	}
+	
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
+	
+	public String getPeriodoLetivo() {
+		return periodoLetivo;
+	}
+	
+	public void setPeriodoLetivo(String periodoLetivo) {
+		this.periodoLetivo = periodoLetivo;
+	}
+	
+	public String getTurma() {
+		return turma;
+	}
+	
+	public void setTurma(String turma) {
+		this.turma = turma;
+	}
+	public String getCargaHoraria() {
+		return cargaHoraria;
+	}
+	
+	public void setCargaHoraria(String cargaHoraria) {
+		this.cargaHoraria = cargaHoraria;
+	}
+}
