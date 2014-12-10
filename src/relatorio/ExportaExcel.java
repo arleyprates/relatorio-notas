d0f1874eedc47b87fc848cd325c894de33e80067
rafaelsandes <rafa_sandes@hotmail.com>
2014-12-10 17:49:24 -0300

    Update ExportaExcel.java

--- src/relatorio/ExportaExcel.java
+++ src/relatorio/ExportaExcel.java
@@ -51,49 +51,68 @@ public class ExportaExcel {
 				String as3 = a3.getContents();
 				al.setNome(as3);
 				
-				Cell a4 = sheet.getCell(3,i);
-				String as4 = a4.getContents();										
-				al.setAvaliacao(as4, 0);
-				//arrayAvaliacao.add(av);
+
 				
-				
+				boolean celulaVazia = false;
+				int h = 3;
+							
+				while (!celulaVazia) {
+					
+					
+						Cell cell = sheet.getCell(h,i);
+						String cellString = cell.getContents();
+						if (cellString.equals("")) {
+							celulaVazia= true;
+							
+						}else{
+							al.setAvaliacao(cellString, h-3);														
+						}
+						
+					
+
+					h++;
+				}				
+
+			
+				/*
 				Cell a5 = sheet.getCell(4,i);
 				String as5 = a5.getContents();							
-				al.setAvaliacao(as5, 1);
+				al.setAvaliacao(as5, h);
 				
 				
 				Cell a6 = sheet.getCell(5,i);
 				String as6 = a6.getContents();				
-				al.setAvaliacao(as6, 2);
+				al.setAvaliacao(as6, h);
 				
 				
 				Cell a7 = sheet.getCell(6,i);
 				String as7 = a7.getContents();								
-				al.setAvaliacao(as7, 3);
+				al.setAvaliacao(as7, h);
 				
 				
 				Cell a8 = sheet.getCell(7, i);
 				String as8 = a8.getContents();				
-				al.setAvaliacao(as8, 4);
+				al.setAvaliacao(as8, h);
 				
 				
 				Cell a9 = sheet.getCell(8, i);
 				String as9 = a9.getContents();				
-				al.setAvaliacao(as9, 5);							
-				
+				al.setAvaliacao(as9, h);
+				}
+				*/
 				//Cell a10 = sheet.getCell(9,i);
 				//String as10 = a10.getContents();
 				//al.setMedia(as10, 5);
 				
 				Float mediaCalculada = (float) 0;
 				
-				for (int j = 0; j < al.getAvaliacao().length; j++) {
+				for (int j = 0; j <= h-5; j++) {
 					mediaCalculada = mediaCalculada + Float.parseFloat(al.getAvaliacao()[j]);
 				}
 								
-				al.setMedia(Float.toString(mediaCalculada/al.getAvaliacao().length));												
+				al.setMedia(Float.toString(mediaCalculada/(h-4)));												
 				
-				float resultado = mediaCalculada/al.getAvaliacao().length;
+				float resultado = mediaCalculada/(h-4);
 				String  result;
 				if (resultado > 5) {
 					result = "Aprovado";			        			
@@ -106,10 +125,10 @@ public class ExportaExcel {
 				System.out.println(al.getResultado());				
 				System.out.println(al.getAvaliacao(0));
 				System.out.println(al.getAvaliacao(1));
-				System.out.println(al.getAvaliacao(2));
-				System.out.println(al.getAvaliacao(3));
-				System.out.println(al.getAvaliacao(4));
-				System.out.println(al.getAvaliacao(5));
+				//System.out.println(al.getAvaliacao(2));
+			//	System.out.println(al.getAvaliacao(3));
+			//	System.out.println(al.getAvaliacao(4));
+			//	System.out.println(al.getAvaliacao(5));
 			
 				//arrayAvaliacao.add(av);
 				
