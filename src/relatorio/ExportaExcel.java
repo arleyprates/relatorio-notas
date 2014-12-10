cc5f4a304b44c5b7daf70dbcfb0094933f10ede8
rafaelsandes <rafa_sandes@hotmail.com>
2014-12-10 16:45:51 -0300

    Update ExportaExcel.java

--- src/relatorio/ExportaExcel.java
+++ src/relatorio/ExportaExcel.java
@@ -3,7 +3,6 @@ package relatorio;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-
 import jxl.Cell;
 import jxl.Sheet;
 import jxl.Workbook;
@@ -23,36 +22,13 @@ public class ExportaExcel {
 		
 		Sheet sheet = workbook.getSheet(0);
 		
-		int linhas = sheet.getRows();
+		int colunas = sheet.getRows();
 		boolean a = false;
 		
 		ArrayList<Aluno> arrayAluno = new ArrayList<Aluno>();
+				
 		
-		Aluno tit = new Aluno();
-			
-		Cell t1 = sheet.getCell(0,0);
-		String ts1 = t1.getContents();
-		tit.setUniversidade(ts1);
-		
-		Cell t2 = sheet.getCell(0,5);
-		String ts2 = t2.getContents();
-		tit.setPeriodoLetivo(ts2);
-		
-		Cell t3 = sheet.getCell(0,6);
-		String ts3 = t3.getContents();
-		tit.setNumeroDisciplina(ts3);
-
-		Cell t4 = sheet.getCell(0,7);
-		String ts4 = t4.getContents();
-		tit.setCargaHoraria(ts4);
-			
-		Cell t5 = sheet.getCell(2,5);
-		String ts5 = t5.getContents();
-		tit.setDisciplina(ts5);
-			
-		arrayAluno.add(tit);
-		
-		for(int i = 0; i < linhas; i++) {			
+		for(int i = 0; i < colunas; i++) {			
 			
 			Cell a1 = sheet.getCell(0,i);
 			String as1 = a1.getContents();
@@ -76,52 +52,58 @@ public class ExportaExcel {
 				al.setNome(as3);
 				
 				Cell a4 = sheet.getCell(3,i);
-				String as4 = a4.getContents();
-				
-				//av.setNota(Float.parseFloat(as4));				
-				
+				String as4 = a4.getContents();										
 				al.setAvaliacao(as4, 0);
 				//arrayAvaliacao.add(av);
 				
 				
 				Cell a5 = sheet.getCell(4,i);
-				String as5 = a5.getContents();
-				//av.setNota(Float.parseFloat(as5));
-				
+				String as5 = a5.getContents();							
 				al.setAvaliacao(as5, 1);
 				
 				
 				Cell a6 = sheet.getCell(5,i);
-				String as6 = a6.getContents();
-				//av.setNota(Float.parseFloat(as6));
-				
+				String as6 = a6.getContents();				
 				al.setAvaliacao(as6, 2);
 				
 				
 				Cell a7 = sheet.getCell(6,i);
-				String as7 = a7.getContents();
-				//av.setNota(Float.parseFloat(as7));
-				
+				String as7 = a7.getContents();								
 				al.setAvaliacao(as7, 3);
 				
 				
 				Cell a8 = sheet.getCell(7, i);
-				String as8 = a8.getContents();
-				
+				String as8 = a8.getContents();				
 				al.setAvaliacao(as8, 4);
 				
 				
 				Cell a9 = sheet.getCell(8, i);
-				String as9 = a9.getContents();
+				String as9 = a9.getContents();				
+				al.setAvaliacao(as9, 5);							
 				
-				al.setAvaliacao(as9, 5);
+				//Cell a10 = sheet.getCell(9,i);
+				//String as10 = a10.getContents();
+				//al.setMedia(as10, 5);
 				
+				Float mediaCalculada = (float) 0;
 				
-				Cell a10 = sheet.getCell(9,i);
-				String as10 = a10.getContents();
-				al.setMedia(as10);		
+				for (int j = 0; j < al.getAvaliacao().length; j++) {
+					mediaCalculada = mediaCalculada + Float.parseFloat(al.getAvaliacao()[j]);
+				}
+								
+				al.setMedia(Float.toString(mediaCalculada/al.getAvaliacao().length));												
 				
+				float resultado = mediaCalculada/al.getAvaliacao().length;
+				String  result;
+				if (resultado > 5) {
+					result = "Aprovado";			        			
+				} else {
+					result = "Reprovado";
+				}
 				
+				al.setResultado(result);
+				
+				System.out.println(al.getResultado());				
 				System.out.println(al.getAvaliacao(0));
 				System.out.println(al.getAvaliacao(1));
 				System.out.println(al.getAvaliacao(2));
@@ -148,6 +130,7 @@ public class ExportaExcel {
 				//System.out.println(x.getNota());
 			//}//
 		//}
+				
 		
 		System.err.println("Total de Alunos Importados: " + arrayAluno.size());
 		workbook.close();
